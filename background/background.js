3f037ba230c52e506a7bba8d24001feb875abee5
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-03-02 01:16:42 +0900

    Show tab-like contents

--- background/background.js
+++ background/background.js
@@ -7,6 +7,9 @@
 
 const TST_ID = 'treestyletab@piro.sakura.ne.jp';
 
+const contentsForTab = new Map();
+const lastActiveForTab = new Map();
+
 async function registerToTST() {
   try {
     await browser.runtime.sendMessage(TST_ID, {
@@ -15,7 +18,7 @@ async function registerToTST() {
       //icons: browser.runtime.getManifest().icons,
       listeningTypes: ['sidebar-show', 'tab-mousedown']
     });
-    setContents();
+    updateAllTabs();
   }
   catch(_error) {
     // TST is not available
@@ -32,13 +35,16 @@ browser.runtime.onMessageExternal.addListener((message, sender) => {
           break;
 
         case 'sidebar-show':
-          setContents({ windowId: message.windowId });
+          updateAllTabs({ windowId: message.windowId });
           break;
 
         case 'tab-mousedown':
-          console.log(message);
           if (message.originalTarget) {
-            return Promise.resolve(true); // cancel default event handling of TST
+            const lastActive = lastActiveForTab.get(message.tab.id);
+            if (lastActive) {
+              browser.tabs.update(lastActive, { active: true });
+              return Promise.resolve(true); // cancel default event handling of TST
+            }
           }
           break;
       }
@@ -46,42 +52,95 @@ browser.runtime.onMessageExternal.addListener((message, sender) => {
   }
 });
 
-browser.tabs.onCreated.addListener(tab => {
-  setContentsTo(tab.id);
+browser.tabs.onRemoved.addListener(tabId => {
+  contentsForTab.delete(tabId);
+  lastActiveForTab.delete(tabId);
 });
 
-async function setContents(options = {}) {
-  const tabs = await browser.tabs.query(options);
-  for (const tab of tabs) {
-    setContentsTo(tab.id);
-  }
-}
+browser.tabs.onActivated.addListener(async activeInfo => {
+  updateTab(activeInfo.tabId);
+  updateTab(activeInfo.previousTabId);
+});
 
-function setContentsTo(tabId) {
+browser.tabs.onUpdated.addListener(async (tabId, _changeInfo, tab) => {
+  updateTab(tabId, tab);
+}, { properties: ['title', 'favIconUrl'] });
+
+
+async function updateTab(tabId, tab = null) {
+  if (!tab)
+    tab = await browser.tabs.get(tabId);
+  if (!tab ||
+      !tab.openerTabId)
+    return;
+
+  const contents = buildContentsForTab(tab);
+  contentsForTab.set(tab.openerTabId, contents);
+  lastActiveForTab.set(tab.openerTabId, tabId);
+
   browser.runtime.sendMessage(TST_ID, {
     type:     'set-extra-tab-contents',
-    id:       tabId,
-    contents: buildContentsForTab(tabId),
+    id:       tab.openerTabId,
+    contents,
     style:    `
       %CONTAINER%:not(.subtree-collapsed) {
         display: none;
       }
-      %CONTAINER% {
-        display: flex;
-        flex-direction: column;
-        flex-wrap: nowrap;
-        justify-content: flex-end;
-      }
+
       %CONTAINER% > .active-tab {
+/*
         display: flex;
         flex-direction: row;
         flex-wrap: nowrap;
         justify-content: flex-start;
+*/
+        border: 1px solid;
+        background: ButtonFace;
+        bottom: 0;
+        color: ButtonText;
+        left: 0;
+        overflow: hidden;
+        position: absolute;
+        right: 0;
+        text-overflow: ".."; /*ellipsis*/;
+        white-space: pre;
+      }
+
+      %CONTAINER% > .active-tab.active {
+        background: ActiveCaption;
+        color: CaptionText;
+      }
+
+      %CONTAINER% img {
+        height: 12px;
+        max-height: 12px;
+        max-width: 12px;
+        width: 12px;
+      }
+
+      %CONTAINER% img[src="#"] {
+        visibility: hidden;
       }
     `
   });
+
+  updateTab(tab.openerTabId);
 }
 
-function buildContentsForTab(tabId) {
-  return `<span class="active-tab">***</span>`;
+function buildContentsForTab(tab) {
+  return `<span class="active-tab ${tab.active ? 'active' : ''}" title="${sanitzeForHTML(tab.title)}"><img src="${tab.favIconUrl}">${sanitzeForHTML(tab.title)}</span>`;
+}
+
+function sanitzeForHTML(string) {
+  return string.replace(/&/g, '&amp;').replace(/\"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+}
+
+async function updateAllTabs(options = {}) {
+  const windows = await (options.windowId ? browser.windows.get(options.windowId, { populate: true }) : browser.windows.getAll({ populate: true }));
+  for (const window of windows) {
+    const tabs = window.tabs.sort((a, b) => a.lastAccessed - b.lastAccessed);
+    for (const tab of tabs) {
+      updateTab(tab.id, tab);
+    }
+  }
 }
